bd2275e916f17f85f374f84bc814f1bd0e255c47
mokariv <91024259+mokariv@users.noreply.github.com>
2021-11-29 00:11:52 +0000

    Sync LeetCode submission - Find Peak Element (java)

--- my-folder/problems/find_peak_element/solution.java
+++ my-folder/problems/find_peak_element/solution.java
@@ -1,52 +1,42 @@
-// class Solution {
-//     public int findPeakElement(int[] nums) {
-
-//         int low =0;
-//         int high = nums.length-1;
-//         int mid;
-//         int size = nums.length-1;
-//         while(low <= high){
-//         mid = low + (high-low)/2;
-//         if(mid>0 && mid<high){
-//         if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
-//             return mid;
-//         }
-//         if(nums[mid-1]>nums[mid]){
-//             high = mid-1;
-//         }
-//         if(nums[mid +1]> nums[mid]){
-//             low = mid+1;
-//         }
-//         }else if(mid == 0){
-//             if(nums[0] > nums[1]){
-//                 return 0;
-//             }else{
-//                 return 1;
-//             }       
-//         }else if(mid == size-1){
-//             if(nums[size-1] > nums[size-2]){
-//                 return size-1;
-//             }else{
-//                 return size-2;
-//             }
-//         }
-//         }
-//     return 0;
-        
-// }
-// }
-
-
-public class Solution {
+class Solution {
     public int findPeakElement(int[] nums) {
-        int l = 0, r = nums.length - 1;
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] > nums[mid + 1])
-                r = mid;
-            else
-                l = mid + 1;
+        if(nums == null || nums.length <=0){
+            return -1;
         }
-        return l;
+        if(nums.length ==1)
+        {
+            return 0;
+        }        
+        int low =0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = low+ (high -low)/2;
+            if(mid == 0){
+                if(nums[mid] > nums[mid+1]){
+                    return 0;
+                }
+                else{
+                    return 1;
+                }
+            }
+            else if(mid == nums.length-1){
+                    if(nums[mid]> nums[mid-1]){
+                        return nums.length -1;
+                    }
+                else{
+                     return nums.length -2;
+                }
+            }
+             else if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }else if(nums[mid] < nums[mid+1]) {
+                 low = mid+1;
+            }else if(nums[mid] < nums[mid-1]){
+                high = mid-1;
+            }
+            
+        }
+    return -1;
+        }
+        
     }
-}
