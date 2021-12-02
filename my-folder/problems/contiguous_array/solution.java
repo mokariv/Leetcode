1405fb2482e4a1ba50d26429effd50acff9c6cf7
mokariv <91024259+mokariv@users.noreply.github.com>
2021-12-02 19:50:42 +0000

    Sync LeetCode submission - Contiguous Array (java)

---
+++ my-folder/problems/contiguous_array/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        if(nums == null || nums.length<=0){
+            return 0;
+        }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0,-1);
+        int max = 0;
+        int sum =0;
+        for(int i =0 ;i< nums.length;i++){
+            if(nums[i] == 0){
+                sum--;
+            }if(nums[i] == 1){
+                sum++;
+            }
+            if(map.containsKey(sum)){
+                max = Math.max(max, i - map.get(sum));
+            }else{
+                map.put(sum,i);
+            }
+        }
+        return max;
+    }
+}
