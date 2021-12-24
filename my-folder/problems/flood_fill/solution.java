e889f80e71316945d3237810bf655c0fc7401b72
mokariv <91024259+mokariv@users.noreply.github.com>
2021-12-24 08:16:34 +0000

    Sync LeetCode submission - Flood Fill (java)

--- my-folder/problems/flood_fill/solution.java
+++ my-folder/problems/flood_fill/solution.java
@@ -1,16 +1,27 @@
 class Solution {
+    int clr;
+    int m, n;
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        if (image[sr][sc] == newColor) return image;
-        fill(image, sr, sc, image[sr][sc], newColor);
+        if(sr> image.length || sr < 0  || sc>image[0].length || image[sr][sc] == newColor){
+            return image;
+        }
+        clr = image[sr][sc];
+        m = image.length;
+        n = image[0].length;
+        dfs(image, sr, sc ,newColor);
         return image;
     }
-    
-    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
-        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) return;
-        image[sr][sc] = newColor;
-        fill(image, sr + 1, sc, color, newColor);
-        fill(image, sr - 1, sc, color, newColor);
-        fill(image, sr, sc + 1, color, newColor);
-        fill(image, sr, sc - 1, color, newColor);
+    private void dfs(int[][] image, int sr, int sc ,int newColor){
+        //base 
+        if(sr < 0 || sc < 0 || sr == m || sc == n || image[sr][sc] != clr){
+            return;
+        }
+    else{
+            image[sr][sc] = newColor;
+            dfs(image, sr+1, sc,   newColor);
+            dfs(image, sr-1, sc,   newColor);
+            dfs(image, sr,   sc+1, newColor);
+            dfs(image, sr,   sc-1, newColor);
+        }
     }
 }
