5f720dcc78073c74c0f497a93494399d2d0d004d
mokariv <91024259+mokariv@users.noreply.github.com>
2021-12-25 07:31:28 +0000

    Sync LeetCode submission - Cousins in Binary Tree (java)

---
+++ my-folder/problems/cousins_in_binary_tree/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode x_parent;
+    TreeNode y_parent;
+    int xLevel;
+    int yLevel;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if(root == null){
+            return false;
+        }
+    dfs(root, x, y, 0, null);
+    return x_parent != y_parent && xLevel == yLevel;
+    }
+    
+    private void dfs(TreeNode root, int x , int y, int level, TreeNode parent){
+        
+        if(root == null){
+            return;
+        }
+        
+        if(root.val == x){
+            x_parent = parent;
+            xLevel = level;
+        }
+        if(root.val == y){
+            y_parent = parent;
+            yLevel = level;
+        }
+        dfs(root.left, x, y, level+1, root);
+        dfs(root.right, x, y,level+1, root);
+        
+        
+    }
+}
