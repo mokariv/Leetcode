03167a16e4527dba270eb2c5e4bd9366050eb3eb
mokariv <91024259+mokariv@users.noreply.github.com>
2021-12-29 10:57:27 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-folder/problems/number_of_islands/solution.java
+++ my-folder/problems/number_of_islands/solution.java
@@ -11,19 +11,19 @@ public int numIslands(char[][] grid) {
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++)
             if (grid[i][j] == '1') {
-                DFSMarking(grid, i, j);
+                dfs(grid, i, j);
                 ++count;
             }
     }    
     return count;
 }
 
-private void DFSMarking(char[][] grid, int i, int j) {
+private void dfs(char[][] grid, int i, int j) {
     if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1' ) return;
     grid[i][j] = '0';
-    DFSMarking(grid, i + 1, j);
-    DFSMarking(grid, i - 1, j);
-    DFSMarking(grid, i, j + 1);
-    DFSMarking(grid, i, j - 1);
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
 }
 }
