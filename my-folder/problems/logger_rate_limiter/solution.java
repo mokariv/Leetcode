dba349d8fcbf11f355631609b4a19a6850912631
mokariv <91024259+mokariv@users.noreply.github.com>
2022-01-02 17:27:27 +0000

    Sync LeetCode submission - Logger Rate Limiter (java)

---
+++ my-folder/problems/logger_rate_limiter/solution.java
@@ -0,0 +1,30 @@
+class Logger {
+    HashMap<String, Integer> map;
+    public Logger() {
+        map = new HashMap<>();
+        
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!map.containsKey(message)){
+            map.put(message, timestamp);
+            return true;
+        }
+        else{
+            int oldtimestamp = map.get(message);
+            if(timestamp - oldtimestamp >= 10){
+                map.put(message,timestamp);
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
