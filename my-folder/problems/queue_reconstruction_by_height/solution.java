968b20b493bbc3da527d4e5099bf0e97e2ef7993
mokariv <91024259+mokariv@users.noreply.github.com>
2022-02-04 21:52:57 +0000

    Sync LeetCode submission - Queue Reconstruction by Height (java)

---
+++ my-folder/problems/queue_reconstruction_by_height/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        if(people == null || people.length == 0){
+            return new int[0][];
+        }
+        Arrays.sort(people,
+                    (a,b)->{
+                    if(a[0] == b[0]){
+                        return a[1] - b[1];
+                    }else{
+                        return b[0] - a[0];
+                    }
+                    }
+                   );
+        List<int[]> result = new ArrayList<>();
+        for(int[] person : people){
+            result.add(person[1], person);
+        }
+        return result.toArray(new int[0][]);
+        
+        
+    }
+}
